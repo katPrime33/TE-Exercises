99a4597d0ac9a49577f06187d14ce0ff3bb32d97
Rachelle Rauh <rachelle.rauh+student@techelevator.com>
2022-07-08 11:30:01 -0400

    Extra Loops and Arrays

--- 04_Loops_and_Arrays/lecture-final/src/main/java/com/techelevator/Lecture.java
+++ 04_Loops_and_Arrays/lecture-final/src/main/java/com/techelevator/Lecture.java
@@ -7,15 +7,15 @@ public class Lecture {
     */
     public int[] returnArray() {
         int[] array = { 80, 8080, 443 };
-        return null;
+        return array;
     }
 
     /*
     2. Return the first element of the array
     */
     public int returnFirstElement() {
-        int[] portNumbers = { 80, 8080, 443 };
-        return 1;
+        int[] portNumbers = new int[] { 80, 8080, 443 };
+        return portNumbers[0];
     }
 
     /*
@@ -23,21 +23,22 @@ public class Lecture {
     */
     public int returnLastElement() {
         int[] portNumbers = { 80, 8080, 443 };
-        return 1;
+        int lastIndex = portNumbers.length-1;
+        return portNumbers[lastIndex];
     }
 
     /*
     4. Return the first element of the array from the parameters
     */
     public int returnFirstElementOfParam(int[] passedInArray) {
-        return 1;
+        return passedInArray[0];
     }
 
     /*
     5. Return the last element of the array from the parameters
     */
     public int returnLastElementOfParam(int[] passedInArray) {
-        return 1;
+        return passedInArray[passedInArray.length-1];
     }
 
     /*
@@ -45,14 +46,14 @@ public class Lecture {
        return it? There are a couple of different ways of doing this, what can you come up with?
     */
     public int returnVariableFromBlock(int number) {
-
+            int result;
         { // A new block with scoped variables
 
-            int result = number * 5;
+            result = number * 5;
 
         } // the result variable disappears here
 
-        return number; // We want to return result here. How?
+        return result; // We want to return result here. How?
     }
 
     /*
@@ -64,10 +65,10 @@ public class Lecture {
 
         {
             int multiplier = 10;
-            result *= multiplier;
+            result *= multiplier; //50
         }
 
-        return result == 1; // <-- Change the number to match result and make this be true
+        return result == 50; // <-- Change the number to match result and make this be true
     }
 
     /*
@@ -88,7 +89,7 @@ public class Lecture {
             double eight = five + three;
         }
 
-        return 0;
+        return one;
     }
 
     /*
@@ -96,7 +97,7 @@ public class Lecture {
     */
     public boolean returnCounterFromLoop() {
 
-        int[] arrayToLoopThrough = { 3, 4, 2, 9 };
+        int[] arrayToLoopThrough = { 3, 4, 2, 9 }; //length is 4
 
         int counter = 0; // Must be started outside the block so that have access to it after the block
 
@@ -104,7 +105,7 @@ public class Lecture {
             counter++;
         }
 
-        return counter == 1; // What should the number be to return true?
+        return counter == 4; // What should the number be to return true?
     }
 
     /*
@@ -116,7 +117,7 @@ public class Lecture {
         int counter = 0;
 
         //     Start;       Keep going while         Increment by one;
-        for (int i = 1; i < arrayToLoopThrough.length; i++) {
+        for (int i = 0; i < arrayToLoopThrough.length; i++) {
             counter += 1;
         }
 

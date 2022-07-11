f3c81e208cdf725b8aaf925c7f8852598fafe583
Rachelle Rauh <rachelle.rauh+student@techelevator.com>
2022-07-11 10:38:25 -0400

    mid lecture push

--- 05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/DiscountCalculator.java
+++ 05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/DiscountCalculator.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+
 import java.util.Scanner;
 
 class DiscountCalculator {
@@ -17,11 +18,17 @@ class DiscountCalculator {
         // The answer needs to be saved as a double
         System.out.print("Enter the discount amount (w/out percentage): ");
 
-
+        String discountAmountFromUser = scanner.nextLine(); //"20"
+        int discountAmount = Integer.parseInt(discountAmountFromUser);
 
         // Prompt the user for a series of prices
         System.out.print("Please provide a series of prices (space separated): ");
 
+        //capture user input from above
+        String pricesFromUser = scanner.nextLine(); // "100 200 1000"
+        String[] pricesArray = pricesFromUser.split(" ");
+        //take prices entered, and apply discount and return new discounted prices
+        
 
 
 

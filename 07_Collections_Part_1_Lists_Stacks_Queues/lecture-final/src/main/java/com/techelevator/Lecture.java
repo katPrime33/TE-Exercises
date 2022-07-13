c496395787e4b6ffe6fea81140fc00233dc0b786
Rachelle Rauh <rachelle.rauh+student@techelevator.com>
2022-07-13 09:01:46 -0400

    Lecture for 07_Collections_Part_1

---
+++ 07_Collections_Part_1_Lists_Stacks_Queues/lecture-final/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,85 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+		System.out.println("####################");
+		System.out.println("       LISTS");
+		System.out.println("####################");
+		List<String> JavaPurple = new ArrayList<>();
+		JavaPurple.add("Aaron");
+		JavaPurple.add("Aileen");
+		JavaPurple.add("Andrew");
+		JavaPurple.add("Arthur");
+		JavaPurple.add("Cody");
+		JavaPurple.add("Dan");
+		JavaPurple.add("Reno");
+		JavaPurple.add("Hulya");
+		JavaPurple.add("James");
+		JavaPurple.add("Jimmy");
+		JavaPurple.add("Kat F.");
+		JavaPurple.add("Kat P.");
+		JavaPurple.add("Laura");
+		JavaPurple.add("Mark");
+		JavaPurple.add("Nate");
+		JavaPurple.add("Nife");
+		JavaPurple.add("Sean");
+		JavaPurple.add("William");
+
+		System.out.println("####################");
+		System.out.println("Lists are ordered");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow duplicates");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow elements to be inserted in the middle");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow elements to be removed by index");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Find out if something is already in the List");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Find index of item in List");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be turned into an array");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be sorted");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be reversed too");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("       FOREACH");
+		System.out.println("####################");
+		System.out.println();
+
+
+	}
+}

a0bacd979d8be1bdeb5cf07d9b2a7cc88dcd72cd
Katie Paulsen <paulsennkatie@gmail.com>
2022-07-17 20:29:50 -0400

    Pushing output-part-09.txt

--- 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -18,11 +18,36 @@ public class Airplane {
         this.totalFirstClassSeats = totalFirstClassSeats;
         this.totalCoachSeats = totalCoachSeats;
     }
-    public Airplane(){
 
+    //derived properties
+
+    public int getAvailableFirstClassSeats(){
+        return totalFirstClassSeats - bookedFirstClassSeats;
+    }
+    public int getAvailableCoachSeats(){
+        return totalCoachSeats - bookedCoachSeats;
     }
 
     //methods
+
+    public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats) {
+        if (forFirstClass == true) {
+            if(totalFirstClassSeats - bookedFirstClassSeats >= totalNumberOfSeats) {
+                bookedFirstClassSeats += totalNumberOfSeats;
+                return true;
+            } else{
+                return false;
+        }
+
+        } else{
+            if(totalCoachSeats - bookedCoachSeats >= totalNumberOfSeats){
+                bookedCoachSeats += totalNumberOfSeats;
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
 
 
 
@@ -30,13 +55,19 @@ public class Airplane {
     //getters for member vars
 
     public String getPlaneNumber(){
-        return this.planeNumber;
+        return planeNumber;
     }
     public int getTotalFirstClassSeats(){
-        return this.getTotalFirstClassSeats();
+        return totalFirstClassSeats;
     }
-    public int getTotalCoachSeats (){
-        return this.getTotalCoachSeats();
+    public int getBookedFirstClassSeats(){
+        return bookedFirstClassSeats;
+    }
+    public int getTotalCoachSeats () {
+        return totalCoachSeats;
+    }
+    public int getBookedCoachSeats(){
+        return bookedCoachSeats;
     }
 
 
--- 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -4,53 +4,75 @@ public class Television {
 
     //member vars
 
-    private boolean isOn;
-    private int currentChannel;
-    private int currentVolume;
+    private boolean isOn = false;
+    private int currentChannel = 3;
+    private int currentVolume = 2;
 
     //methods
 
     public void turnOff() {
-
+        isOn = false;
     }
 
     public void turnOn(){
-
+        isOn = true;
+        currentChannel = 3;
+        currentVolume = 2;
     }
 
     public void changeChannel(int newChannel){
-
+        if(isOn && newChannel > 3 && newChannel < 18){
+            currentChannel = newChannel;
+        }
     }
 
     public void channelUp(){
-
+        if(isOn){
+            if(currentChannel == 18){
+                currentChannel = 3;
+            }
+            else{
+                currentChannel++;
+            }
+        }
     }
 
     public void channelDown(){
-
+        if(isOn){
+            if(currentChannel == 3){
+                currentChannel = 18;
+            }
+            else{
+                currentChannel--;
+            }
+        }
     }
 
     public void raiseVolume(){
-
+        if(isOn && currentVolume < 10){
+            currentVolume++;
+        }
     }
 
     public void lowerVolume(){
-
+        if(isOn && currentVolume > 0){
+            currentVolume--;
+        }
     }
 
 
-    //getters for member vars
+    //getters
 
     public boolean isOn(){
-        return this.isOn;
+        return isOn;
     }
 
-    public int currentChannel(){
-        return this.currentChannel;
+    public int getCurrentChannel(){
+        return currentChannel;
     }
 
-    public int currentVolume(){
-        return this.currentVolume;
+    public int getCurrentVolume(){
+        return currentVolume;
     }
 
 

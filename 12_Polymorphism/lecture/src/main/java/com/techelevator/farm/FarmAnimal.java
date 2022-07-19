7ab562ea3ca1aa1b1e1cfa8eacd58efa3ca8ab70
Rachelle Rauh <rachelle.rauh+student@techelevator.com>
2022-07-19 08:19:03 -0400

    adding polymorphism lecture and exercise

---
+++ 12_Polymorphism/lecture/src/main/java/com/techelevator/farm/FarmAnimal.java
@@ -0,0 +1,19 @@
+package com.techelevator.farm;
+
+public class FarmAnimal {
+	private String name;
+	private String sound;
+
+	public FarmAnimal(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSound() {
+		return sound;
+	}
+
+}

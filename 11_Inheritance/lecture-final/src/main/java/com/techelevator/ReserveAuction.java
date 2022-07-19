3c38aa7958881cc83fde202fd10a7de318feaaa7
Rachelle Rauh <rachelle.rauh+student@techelevator.com>
2022-07-18 20:56:41 -0400

    pushing polymorphism tutorial

--- 11_Inheritance/lecture-final/src/main/java/com/techelevator/ReserveAuction.java
+++ 11_Inheritance/lecture-final/src/main/java/com/techelevator/ReserveAuction.java
@@ -11,11 +11,18 @@ public class ReserveAuction extends Auction {
         this.reservePrice = reservePrice;
     }
 
+
+
+    /*
+    This class overrides the default placeBid behavior of the parent class.
+    If the offeredBid does not meet or exceed the reserve price,
+    the bid is ignored.
+     */
     @Override
     public boolean placeBid(Bid offeredBid) {
         boolean isCurrentWinningBid = false;
         if(offeredBid.getBidAmount() >= reservePrice){
-            isCurrentWinningBid = super.placeBid(offeredBid);
+            isCurrentWinningBid = super.placeBid(offeredBid); //invokes placeBid method on super class
         }
         return isCurrentWinningBid;
     }

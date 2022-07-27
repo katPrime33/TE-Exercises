27927b2c6e3f8c3c0842f0d0c77dd3110e518bab
Katie Paulsen <paulsennkatie@gmail.com>
2022-07-27 18:07:17 -0400

    Repushing-due-to-build-error.txt

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -17,14 +17,14 @@ public class FizzWriter {
 
 		String output;
 		try(PrintWriter dataOutput = new PrintWriter(destinationFile)){
-			for(int i = 1; i <= 300; i++){
-				if(i % 3 == 0 || Integer.toString(i).contains("3")){
+			for(int i = 1; i <= 300; i++) {
+				if (i % 3 == 0 || Integer.toString(i).contains("3")) {
 					output = "Fizz";
-				} else if(i % 5 == 0 || Integer.toString(i).contains("5")){
+				} else if (i % 5 == 0 || Integer.toString(i).contains("5")) {
 					output = "Buzz";
-				} else if(i % 15 == 0) {
+				} else if (i % 15 == 0) {
 					output = "FizzBuzz";
-				}else{
+				} else {
 					output = Integer.toString(i);
 				}
 				dataOutput.println(output);

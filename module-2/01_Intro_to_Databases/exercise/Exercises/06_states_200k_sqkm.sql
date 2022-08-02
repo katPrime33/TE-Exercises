36fd5de6ab330e1d4cc9e463662718ea03a184e6
Katie Paulsen <paulsennkatie@gmail.com>
2022-08-02 07:48:48 -0400

    Commiting module 2 exercise 1 to avoid merge error

--- module-2/01_Intro_to_Databases/exercise/Exercises/06_states_200k_sqkm.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/06_states_200k_sqkm.sql
@@ -1,2 +1,4 @@
 -- 6. The name, abbreviation, population, and area of states with an area greater than 200,000 square kilometers (16 rows)
-
+SELECT state_name, population, area, state_abbreviation
+FROM state
+WHERE area > 200000;
--- module-2/01_Intro_to_Databases/exercise/Exercises/19_population_density_all.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/19_population_density_all.sql
@@ -1,3 +1,6 @@
--- 19. The name, population, area, and population density (name the column 'population_density') of all states, territories, and districts (56 rows)
+-- 19. The name, population, area, and population density (name the column 'population_density') of
+--all states, territories, and districts (56 rows)
 -- Population density is expressed as people per square kilometer. In other words, population divided by area.
-
+SELECT population, area, state_name
+FROM state
+WHERE population/area = population_density;

613a20162a63cf57a4979fd9fa8f13179e561acb
Rachelle Rauh <rachelle.rauh+student@techelevator.com>
2022-09-15 09:55:15 -0400

    starting lecture for component communication

---
+++ module-3/13_Vue_Component_Communication/lecture-final/src/components/AverageSummary.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="well" v-on:click="updateFilter">
+        <span class="amount">{{ averageRating }}</span>
+        Average Rating
+      </div>
+</template>
+
+<script>
+export default {
+    name: "average-summary",
+    methods: {
+        updateFilter(){
+            this.$store.commit("UPDATE_FILTER", 0);
+        }
+    },
+    computed: {
+         averageRating() {
+      let sum = this.$store.state.reviews.reduce((currentSum, review) => {
+        return currentSum + review.rating;
+      }, 0);
+      return (sum / this.$store.state.reviews.length).toFixed(2);
+    },
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ module-3/13_Vue_Component_Communication/lecture/src/components/StarSummary.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="well" v-on:click="updateFilter()">
+        <span class="amount">{{ numberReviews }}</span>
+        {{ rating }} Star Review{{ numberOfReviews === 1 ? "" : "s" }}
+      </div>
+</template>
+
+<script>
+export default {
+  name: "star-summary",
+  props: ["rating"],
+  methods: {
+    updateFilter() {
+
+    }
+  },
+  computed: {
+    numberOfReviews() {
+      const reviews = [];
+      return reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === parseInt(this.rating) ? 1 : 0);
+      }, 0);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
